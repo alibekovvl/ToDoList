94e6a3f0cb71d050098cfe9c808fb4c1795dcdb7
MSI <aidemiralibekov@mail.ru>
2025-02-06 18:33:37 +0300

    Initial commit

---
+++ ToDoList.Domain/Enum/Priority.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+/// <summary>
+/// Приоритет задачи 
+/// </summary>
+namespace ToDoList.Domain.Enum
+{
+    public  enum Priority
+    {
+        [Display(Name = "Простая")]
+        Easy = 0,
+        [Display(Name = "Важная")]
+        Medium = 1,
+        [Display(Name = "Критичная")]
+        High = 2
+    }
+}
